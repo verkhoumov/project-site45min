08dc5f55e595b94a20f15dea001f65d4641e6454
verkhoumov <verkhoumov@gmail.com>
2017-01-10 18:08:06 +0300

    Check some small bug "v"
    Delete "v" symbol from site footer.

--- project/www/application/views/templates/select.php
+++ project/www/application/views/templates/select.php
@@ -2,4 +2,4 @@
 	<img class="feedback-user-photo" src="{{avatar}}" alt="{{name}}">
 	<span class="feedback-user-name overflow">{{name}}</span>
 	{{#email}}<span class="feedback-user-email overflow">{{email}}</span>{{/email}}
-</script>v
+</script>
